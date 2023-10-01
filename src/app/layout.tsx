af6224e60e57346a4588b79c16cf47ddf1e38d34
victorkane <victorkane@gmail.com>
2023-10-01 07:18:05 -0300

    chore(NavBar): scaffold

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils"
 import { Inter } from "next/font/google"
 import type { Metadata } from "next"
 import "./globals.css"
+import Navbar from "@/components/NavBar"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           inter.className
         )}
       >
+        <Navbar />
         {children}
       </body>
     </html>
